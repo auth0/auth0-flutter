7feb6ddd1777eb58f73219fd771d6b1b4b80d1bd
frederikprijck <frederik.prijck@auth0.com>
2022-03-22 12:14:34 +0100

    Include generates files when running on OSX

--- auth0_flutter/ios/Classes/SwiftAuth0FlutterPlugin.swift
+++ auth0_flutter/ios/Classes/SwiftAuth0FlutterPlugin.swift
@@ -9,8 +9,6 @@ public class SwiftAuth0FlutterPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-
     if (call.method == "getPlatformVersion") {
       result("iOS " + UIDevice.current.systemVersion)
     } else if(call.method == "login") {
