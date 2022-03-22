40b7456a001b54ed16d701c6477fa68fc3cf90a0
Frederik Prijck <frederik.prijck@gmail.com>
2022-03-22 20:20:48 +0100

    Update auth0_flutter/ios/Classes/SwiftAuth0FlutterPlugin.swift
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>

--- auth0_flutter/ios/Classes/SwiftAuth0FlutterPlugin.swift
+++ auth0_flutter/ios/Classes/SwiftAuth0FlutterPlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public class SwiftAuth0FlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "plugins.flutter.io/auth0_flutter", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "auth0.com/auth0_flutter", binaryMessenger: registrar.messenger())
     let instance = SwiftAuth0FlutterPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
