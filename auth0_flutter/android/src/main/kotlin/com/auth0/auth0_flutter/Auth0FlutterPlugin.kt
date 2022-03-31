bedbb9cca1228b0319896409e25ce620781f912e
Steve Hobbs <steve.hobbs@hey.com>
2022-03-31 20:55:29 +0100

    feat: web auth login on Android

--- auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/Auth0FlutterPlugin.kt
+++ auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/Auth0FlutterPlugin.kt
@@ -1,7 +1,11 @@
 package com.auth0.auth0_flutter
 
+import android.content.Context
+import android.util.Log
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -9,18 +13,18 @@ import io.flutter.plugin.common.MethodChannel.Result
 
 
 /** Auth0FlutterPlugin */
-class Auth0FlutterPlugin: FlutterPlugin, MethodCallHandler {
+class Auth0FlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var webAuthMethodChannel : MethodChannel
   private lateinit var authMethodChannel : MethodChannel
-
+  private val webAuthCallHandler = Auth0FlutterWebAuthMethodCallHandler()
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     webAuthMethodChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "auth0.com/auth0_flutter/web_auth")
-    webAuthMethodChannel.setMethodCallHandler(Auth0FlutterWebAuthMethodCallHandler())
+    webAuthMethodChannel.setMethodCallHandler(webAuthCallHandler)
 
     authMethodChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "auth0.com/auth0_flutter/auth")
     authMethodChannel.setMethodCallHandler(Auth0FlutterAuthMethodCallHandler())
@@ -31,5 +35,21 @@ class Auth0FlutterPlugin: FlutterPlugin, MethodCallHandler {
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     webAuthMethodChannel.setMethodCallHandler(null)
     authMethodChannel.setMethodCallHandler(null)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    webAuthCallHandler.context = binding.activity
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    TODO("Not yet implemented")
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    TODO("Not yet implemented")
+  }
+
+  override fun onDetachedFromActivity() {
+    TODO("Not yet implemented")
   }
 }
--- auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/Auth0FlutterWebAuthMethodCallHandler.kt
+++ auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/Auth0FlutterWebAuthMethodCallHandler.kt
@@ -1,25 +1,53 @@
 package com.auth0.auth0_flutter
 
+import android.content.Context
+import android.util.Log
 import androidx.annotation.NonNull
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.Callback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 
-class Auth0FlutterWebAuthMethodCallHandler: MethodCallHandler {
+class Auth0FlutterWebAuthMethodCallHandler : MethodCallHandler {
     private val WEBAUTH_LOGIN_METHOD = "webAuth#login"
     private val WEBAUTH_LOGOUT_METHOD = "webAuth#logout"
+    lateinit var context: Context
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+
+        val callback = object : Callback<Credentials, AuthenticationException> {
+            override fun onFailure(exception: AuthenticationException) {
+                // Failure! Check the exception for details
+            }
+
+            override fun onSuccess(credentials: Credentials) {
+                // Success! Access token and ID token are presents
+                val scope = credentials.scope?.split(" ") ?: listOf()
+
+                result.success(mapOf(
+                    "accessToken" to credentials.accessToken,
+                    "idToken" to credentials.idToken,
+                    "refreshToken" to credentials.refreshToken,
+                    "userProfile" to mapOf<String, String>(),
+                    "expiresAt" to credentials.expiresAt.time,
+                    "scopes" to scope
+                ))
+            }
+        }
+
         when (call.method) {
             WEBAUTH_LOGIN_METHOD -> {
-                result.success(mapOf(
-                    "accessToken" to "AccessToken",
-                    "idToken" to "IdToken",
-                    "refreshToken" to "RefreshToken",
-                    "userProfile" to mapOf("name" to "John Doe"),
-                    "expiresIn" to 10,
-                    "scopes" to listOf("a", "b")
-                ))
+                Log.d("DEBUG", context.packageName)
+                WebAuthProvider
+                    .login(Auth0("dk1lvC9QyrdALZeXNaiIOwjX9BlMNmpv", "elkdanger.eu.auth0.com"))
+                    .withScheme("demo")
+                    .start(context, callback)
             }
             WEBAUTH_LOGOUT_METHOD -> {
                 result.success("Web Auth Logout Success")
