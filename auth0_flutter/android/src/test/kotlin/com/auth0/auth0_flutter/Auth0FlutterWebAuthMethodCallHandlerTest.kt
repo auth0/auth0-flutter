b6854fc69aa880304a5a624c57e259f8e70f956a
Steve Hobbs <steve.hobbs@auth0.com>
2022-04-08 14:01:07 +0100

    chore: initial test setup

---
+++ auth0_flutter/android/src/test/kotlin/com/auth0/auth0_flutter/Auth0FlutterWebAuthMethodCallHandlerTest.kt
@@ -0,0 +1,15 @@
+package com.auth0.auth0_flutter
+
+//import kotlin.test.assertEquals
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
+import org.junit.runner.RunWith
+
+class Auth0FlutterWebAuthMethodCallHandlerTest {
+
+    @org.junit.Test
+    fun onMethodCall() {
+        println("WORKING")
+        assertThat(1 == 1, equalTo(true))
+    }
+}
