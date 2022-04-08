83d9288f3ccac362fbb503d7c8357f030b2a4aaa
Frederik <frederik.prijck@gmail.com>
2022-04-08 18:17:22 +0200

    Add missing import

--- auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/request_handlers/api/RenewAccessTokenApiRequestHandler.kt
+++ auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/request_handlers/api/RenewAccessTokenApiRequestHandler.kt
@@ -3,6 +3,7 @@ package com.auth0.auth0_flutter.request_handlers.api
 import com.auth0.android.authentication.AuthenticationAPIClient
 import com.auth0.android.authentication.AuthenticationException
 import com.auth0.android.callback.Callback
+import com.auth0.android.jwt.JWT
 import com.auth0.android.result.Credentials
 import com.auth0.auth0_flutter.request_handlers.MethodCallRequest
 import com.auth0.auth0_flutter.toMap
