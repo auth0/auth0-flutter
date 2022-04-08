507ef6b49d98bcdb473feb095fd94486ef6702a7
Frederik <frederik.prijck@gmail.com>
2022-04-08 12:22:37 +0200

    Store errorFlags seperatly and remove from ApiException details

--- auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/AuthenticationExceptionExtensions.kt
+++ auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/AuthenticationExceptionExtensions.kt
@@ -2,28 +2,30 @@ package com.auth0.auth0_flutter
 
 import com.auth0.android.authentication.AuthenticationException
 
-fun AuthenticationException.toMap(): Map<String, Boolean> {
+fun AuthenticationException.toMap(): Map<String, Any> {
     return mapOf(
-        "isMultifactorRequired" to this.isMultifactorRequired,
-        "isMultifactorEnrollRequired" to this.isMultifactorEnrollRequired,
-        "isMultifactorCodeInvalid" to this.isMultifactorCodeInvalid,
-        "isMultifactorTokenInvalid" to this.isMultifactorTokenInvalid,
-        "isPasswordNotStrongEnough" to this.isPasswordNotStrongEnough,
-        "isPasswordAlreadyUsed" to this.isPasswordAlreadyUsed,
-        "isRuleError" to this.isRuleError,
-        "isInvalidCredentials" to this.isInvalidCredentials,
-        "isRefreshTokenDeleted" to this.isRefreshTokenDeleted,
-        "isAccessDenied" to this.isAccessDenied,
-        "isTooManyAttempts" to this.isTooManyAttempts,
-        "isVerificationRequired" to this.isVerificationRequired,
-        "isNetworkError" to this.isNetworkError,
-        "isBrowserAppNotAvailable" to this.isBrowserAppNotAvailable,
-        "isPKCENotAvailable" to this.isPKCENotAvailable,
-        "isInvalidAuthorizeURL" to this.isInvalidAuthorizeURL,
-        "isInvalidConfiguration" to this.isInvalidConfiguration,
-        "isCanceled" to this.isCanceled,
-        "isPasswordLeaked" to this.isPasswordLeaked,
-        "isLoginRequired" to this.isLoginRequired,
+        "_errorFlags" to mapOf(
+            "isMultifactorRequired" to this.isMultifactorRequired,
+            "isMultifactorEnrollRequired" to this.isMultifactorEnrollRequired,
+            "isMultifactorCodeInvalid" to this.isMultifactorCodeInvalid,
+            "isMultifactorTokenInvalid" to this.isMultifactorTokenInvalid,
+            "isPasswordNotStrongEnough" to this.isPasswordNotStrongEnough,
+            "isPasswordAlreadyUsed" to this.isPasswordAlreadyUsed,
+            "isRuleError" to this.isRuleError,
+            "isInvalidCredentials" to this.isInvalidCredentials,
+            "isRefreshTokenDeleted" to this.isRefreshTokenDeleted,
+            "isAccessDenied" to this.isAccessDenied,
+            "isTooManyAttempts" to this.isTooManyAttempts,
+            "isVerificationRequired" to this.isVerificationRequired,
+            "isNetworkError" to this.isNetworkError,
+            "isBrowserAppNotAvailable" to this.isBrowserAppNotAvailable,
+            "isPKCENotAvailable" to this.isPKCENotAvailable,
+            "isInvalidAuthorizeURL" to this.isInvalidAuthorizeURL,
+            "isInvalidConfiguration" to this.isInvalidConfiguration,
+            "isCanceled" to this.isCanceled,
+            "isPasswordLeaked" to this.isPasswordLeaked,
+            "isLoginRequired" to this.isLoginRequired,
+        )
     )
 }
 
--- auth0_flutter_platform_interface/lib/src/auth/api_exception.dart
+++ auth0_flutter_platform_interface/lib/src/auth/api_exception.dart
@@ -1,45 +1,70 @@
 import 'package:flutter/services.dart';
 
+import '../extensions/map_extensions.dart';
+
 class ApiException implements Exception {
   static const _unknown = 'UNKNOWN';
 
   final String code;
   final String message;
   final Map<String, dynamic> details;
+  final Map<dynamic, dynamic> _errorFlags;
 
-  const ApiException(this.code, this.message, this.details);
+  const ApiException(this.code, this.message, this.details, this._errorFlags);
   const ApiException.unknown(this.message)
       : code = ApiException._unknown,
-        details = const {};
-  factory ApiException.fromPlatformException(final PlatformException e) =>
-      ApiException(
-          e.code,
-          e.message ?? '', // Errors from native should always have a message
-          Map<String, dynamic>.from(
-              (e.details ?? <dynamic, dynamic>{}) as Map<dynamic, dynamic>));
+        details = const {},
+        _errorFlags = const {};
+  factory ApiException.fromPlatformException(final PlatformException e) {
+    final details = Map<String, dynamic>.from(
+        (e.details ?? <dynamic, dynamic>{}) as Map<dynamic, dynamic>);
+    final errorFlags = details['_errorFlags'] as Map<dynamic, dynamic>;
+
+    details.remove('_errorFlags');
+
+    return ApiException(
+        e.code,
+        e.message ?? '', // Errors from native should always have a message
+        details,
+        errorFlags);
+  }
 
   @override
   String toString() => '$code: $message';
 
-  bool get isMultifactorRequired => details['isMultifactorRequired'] as bool;
-  bool get isMultifactorEnrollRequired => details['isMultifactorEnrollRequired'] as bool;
-  bool get isMultifactorCodeInvalid => details['isMultifactorCodeInvalid'] as bool;
-  bool get isMultifactorTokenInvalid => details['isMultifactorTokenInvalid'] as bool;
-  bool get isPasswordNotStrongEnough => details['isPasswordNotStrongEnough'] as bool;
-  bool get isPasswordAlreadyUsed => details['isPasswordAlreadyUsed'] as bool;
-  bool get isRuleError => details['isRuleError'] as bool;
-  bool get isInvalidCredentials => details['isInvalidCredentials'] as bool;
-  bool get isRefreshTokenDeleted => details['isRefreshTokenDeleted'] as bool;
-  bool get isAccessDenied => details['isAccessDenied'] as bool;
-  bool get isTooManyAttempts => details['isTooManyAttempts'] as bool;
-  bool get isVerificationRequired => details['isVerificationRequired'] as bool;
-  bool get isNetworkError => details['isNetworkError'] as bool;
-  bool? get isBrowserAppNotAvailable => details['isBrowserAppNotAvailable'] as bool?;
-  bool get isPKCENotAvailable => details['isPKCENotAvailable'] as bool;
-  bool get isInvalidAuthorizeURL => details['isInvalidAuthorizeURL'] as bool;
-  bool get isInvalidConfiguration => details['isInvalidConfiguration'] as bool;
-  bool get isCanceled => details['isCanceled'] as bool;
-  bool get isPasswordLeaked => details['isPasswordLeaked'] as bool;
-  bool get isLoginRequired => details['isLoginRequired'] as bool;
-
+  bool get isMultifactorRequired =>
+      _errorFlags.getBooleanOrFalse('isMultifactorRequired');
+  bool get isMultifactorEnrollRequired =>
+      _errorFlags.getBooleanOrFalse('isMultifactorEnrollRequired');
+  bool get isMultifactorCodeInvalid =>
+      _errorFlags.getBooleanOrFalse('isMultifactorCodeInvalid');
+  bool get isMultifactorTokenInvalid =>
+      _errorFlags.getBooleanOrFalse('isMultifactorTokenInvalid');
+  bool get isPasswordNotStrongEnough =>
+      _errorFlags.getBooleanOrFalse('isPasswordNotStrongEnough');
+  bool get isPasswordAlreadyUsed =>
+      _errorFlags.getBooleanOrFalse('isPasswordAlreadyUsed');
+  bool get isRuleError => _errorFlags.getBooleanOrFalse('isRuleError');
+  bool get isInvalidCredentials =>
+      _errorFlags.getBooleanOrFalse('isInvalidCredentials');
+  bool get isRefreshTokenDeleted =>
+      _errorFlags.getBooleanOrFalse('isRefreshTokenDeleted');
+  bool get isAccessDenied => _errorFlags.getBooleanOrFalse('isAccessDenied');
+  bool get isTooManyAttempts =>
+      _errorFlags.getBooleanOrFalse('isTooManyAttempts');
+  bool get isVerificationRequired =>
+      _errorFlags.getBooleanOrFalse('isVerificationRequired');
+  bool get isNetworkError => _errorFlags.getBooleanOrFalse('isNetworkError');
+  bool get isBrowserAppNotAvailable =>
+      _errorFlags.getBooleanOrFalse('isBrowserAppNotAvailable');
+  bool get isPKCENotAvailable =>
+      _errorFlags.getBooleanOrFalse('isPKCENotAvailable');
+  bool get isInvalidAuthorizeURL =>
+      _errorFlags.getBooleanOrFalse('isInvalidAuthorizeURL');
+  bool get isInvalidConfiguration =>
+      _errorFlags.getBooleanOrFalse('isInvalidConfiguration');
+  bool get isCanceled => _errorFlags.getBooleanOrFalse('isCanceled');
+  bool get isPasswordLeaked =>
+      _errorFlags.getBooleanOrFalse('isPasswordLeaked');
+  bool get isLoginRequired => _errorFlags.getBooleanOrFalse('isLoginRequired');
 }
