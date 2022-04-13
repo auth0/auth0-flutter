fd743ed428c63fbb37607ad565ff91a5d1398cd7
Steve Hobbs <steve.hobbs@auth0.com>
2022-04-13 21:39:49 +0100

    chore: individual handlers now accept builder vs creating it themselves

--- auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/request_handlers/web_auth/LoginWebAuthRequestHandler.kt
+++ auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/request_handlers/web_auth/LoginWebAuthRequestHandler.kt
@@ -3,76 +3,69 @@ package com.auth0.auth0_flutter.request_handlers.web_auth
 import android.content.Context
 import com.auth0.android.authentication.AuthenticationException
 import com.auth0.android.callback.Callback
+import com.auth0.android.jwt.JWT
 import com.auth0.android.provider.WebAuthProvider
 import com.auth0.android.result.Credentials
+import com.auth0.auth0_flutter.createUserProfileFromClaims
 import com.auth0.auth0_flutter.request_handlers.MethodCallRequest
+import com.auth0.auth0_flutter.toMap
 import io.flutter.plugin.common.MethodChannel
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
-import com.auth0.android.jwt.JWT
-import com.auth0.auth0_flutter.createUserProfileFromClaims
-import com.auth0.auth0_flutter.toMap
 
-
-private val WEBAUTH_LOGIN_METHOD = "webAuth#login"
-
-class LoginWebAuthRequestHandler : WebAuthRequestHandler {
-    override val method: String = WEBAUTH_LOGIN_METHOD
-
+class LoginWebAuthRequestHandler(private val builder: WebAuthProvider.Builder) :
+    WebAuthRequestHandler {
     override fun handle(
         context: Context,
         request: MethodCallRequest,
         result: MethodChannel.Result
     ) {
-        val loginBuilder = WebAuthProvider.login(request.account)
-
-        val args = request.data;
+        val args = request.data
         val scopes = args.getOrDefault("scopes", arrayListOf<String>()) as ArrayList<*>
+
         if (scopes.isNotEmpty()) {
-            loginBuilder.withScope(scopes.joinToString(separator = " "))
+            builder.withScope(scopes.joinToString(separator = " "))
         }
 
         if (args.getOrDefault("audience", null) is String) {
-            loginBuilder.withAudience(args["audience"] as String)
+            builder.withAudience(args["audience"] as String)
         }
 
         if (args.getOrDefault("redirectUri", null) is String) {
-            loginBuilder.withRedirectUri(args["redirectUri"] as String)
+            builder.withRedirectUri(args["redirectUri"] as String)
         }
 
         if (args.getOrDefault("organizationId", null) is String) {
-            loginBuilder.withOrganization(args["organizationId"] as String)
+            builder.withOrganization(args["organizationId"] as String)
         }
 
         if (args.getOrDefault("invitationUrl", null) is String) {
-            loginBuilder.withInvitationUrl(args["invitationUrl"] as String)
+            builder.withInvitationUrl(args["invitationUrl"] as String)
         }
 
         if (args.getOrDefault("leeway", null) is Int) {
-            loginBuilder.withIdTokenVerificationLeeway(args["leeway"] as Int)
+            builder.withIdTokenVerificationLeeway(args["leeway"] as Int)
         }
 
         if (args.getOrDefault("maxAge", null) is Int) {
-            loginBuilder.withMaxAge(args["maxAge"] as Int)
+            builder.withMaxAge(args["maxAge"] as Int)
         }
 
         if (args.getOrDefault("issuer", null) is String) {
-            loginBuilder.withIdTokenVerificationIssuer(args["issuer"] as String)
+            builder.withIdTokenVerificationIssuer(args["issuer"] as String)
         }
 
         if (args.getOrDefault("scheme", null) is String) {
-            loginBuilder.withScheme(args["scheme"] as String)
+            builder.withScheme(args["scheme"] as String)
         }
 
-        if (args.getOrDefault("parameters", hashMapOf<String, Any?>()) is HashMap<*, *>) {
-            loginBuilder.withParameters(args["parameters"] as HashMap<String, *>)
+        if (args.getOrDefault("parameters", null) is HashMap<*, *>) {
+            builder.withParameters(args["parameters"] as HashMap<String, *>)
         }
 
-        loginBuilder.start(context, object : Callback<Credentials, AuthenticationException> {
+        builder.start(context, object : Callback<Credentials, AuthenticationException> {
             override fun onFailure(exception: AuthenticationException) {
-                result.error(exception.getCode(), exception.getDescription(), exception);
+                result.error(exception.getCode(), exception.getDescription(), exception)
             }
 
             override fun onSuccess(credentials: Credentials) {
@@ -100,6 +93,4 @@ class LoginWebAuthRequestHandler : WebAuthRequestHandler {
             }
         })
     }
-
-
 }
