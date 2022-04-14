f195227e4659a9bac6bb9fe027b26ca4c82fe982
Steve Hobbs <steve.hobbs@auth0.com>
2022-04-14 13:21:35 +0100

    chore: refactor web auth tests to extract common code

--- auth0_flutter/android/src/test/kotlin/com/auth0/auth0_flutter/Auth0FlutterWebAuthMethodCallHandlerTest.kt
+++ auth0_flutter/android/src/test/kotlin/com/auth0/auth0_flutter/Auth0FlutterWebAuthMethodCallHandlerTest.kt
@@ -1,12 +1,13 @@
 package com.auth0.auth0_flutter
 
 //import kotlin.test.assertEquals
-import android.content.Context
 import com.auth0.android.authentication.AuthenticationException
 import com.auth0.android.callback.Callback
 import com.auth0.android.provider.WebAuthProvider
 import com.auth0.android.result.Credentials
+import com.auth0.auth0_flutter.request_handlers.MethodCallRequest
 import com.auth0.auth0_flutter.request_handlers.web_auth.LoginWebAuthRequestHandler
+import com.auth0.auth0_flutter.request_handlers.web_auth.WebAuthRequestHandler
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel.Result
 import org.hamcrest.CoreMatchers.*
@@ -20,13 +21,12 @@ import java.text.SimpleDateFormat
 
 @RunWith(RobolectricTestRunner::class)
 class Auth0FlutterWebAuthMethodCallHandlerTest {
-    @Test
-    fun `handler should result in 'notImplemented' if no handler`() {
+    fun runCallHandler(resolver: (MethodCall, MethodCallRequest) -> WebAuthRequestHandler?, onResult: (Result) -> Unit) {
         val handler = Auth0FlutterWebAuthMethodCallHandler()
         val mockResult = mock<Result>()
 
         handler.context = mock()
-        handler.handlerResolver = { call, request -> null }
+        handler.handlerResolver = resolver
 
         val args = hashMapOf(
             "domain" to "test.auth0.com",
@@ -34,51 +34,45 @@ class Auth0FlutterWebAuthMethodCallHandlerTest {
         )
 
         handler.onMethodCall(MethodCall("webAuth#login", args), mockResult)
+        onResult(mockResult)
+    }
 
-        verify(mockResult).notImplemented()
+    @Test
+    fun `handler should result in 'notImplemented' if no handler`() {
+        runCallHandler({ _, _ -> null }, { result ->
+            verify(result).notImplemented()
+        })
     }
 
     @Test
     fun `handler should log in using the Auth0 SDK`() {
-        val handler = Auth0FlutterWebAuthMethodCallHandler()
-        val mockResult = mock<Result>()
+        val credentials = Credentials(JwtTestUtils.createJwt(), "test", "", null, Date(), null)
 
-        handler.context = mock()
-
-        handler.handlerResolver = { call, request ->
+        runCallHandler({ _, _ ->
             val builder = mock<WebAuthProvider.Builder>()
 
             doAnswer { invocation ->
                 val callback =
                     invocation.getArgument<Callback<Credentials, AuthenticationException>>(1)
 
-                val credentials = Credentials(JwtTestUtils.createJwt(), "test", "", null, Date(), null)
-
                 callback.onSuccess(credentials)
-
-                val sdf =
-                    SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.getDefault())
-
-                val formattedDate = sdf.format(credentials.expiresAt)
-
-                verify(mockResult).success(check {
-                    val map = it as Map<*, *>
-                    assertThat(map["idToken"], equalTo(credentials.idToken))
-                    assertThat(map["accessToken"], equalTo(credentials.accessToken))
-                    assertThat(map["expiresAt"], equalTo(formattedDate))
-                    assertThat(map["scope"], equalTo(credentials.scope))
-                    assertThat(map["refreshToken"], nullValue())
-                })
             }.`when`(builder).start(any(), any())
 
             LoginWebAuthRequestHandler(builder)
-        }
+        }, { result ->
+            val sdf =
+                SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.getDefault())
 
-        val args = hashMapOf(
-            "domain" to "test.auth0.com",
-            "clientId" to "test-client"
-        )
+            val formattedDate = sdf.format(credentials.expiresAt)
 
-        handler.onMethodCall(MethodCall("webAuth#login", args), mockResult)
+            verify(result).success(check {
+                val map = it as Map<*, *>
+                assertThat(map["idToken"], equalTo(credentials.idToken))
+                assertThat(map["accessToken"], equalTo(credentials.accessToken))
+                assertThat(map["expiresAt"], equalTo(formattedDate))
+                assertThat(map["scope"], equalTo(credentials.scope))
+                assertThat(map["refreshToken"], nullValue())
+            })
+        })
     }
 }
