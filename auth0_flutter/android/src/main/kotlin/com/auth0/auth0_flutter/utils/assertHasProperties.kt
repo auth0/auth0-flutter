b3df06893a2316b8ab71be769aae1c60c63163a2
Frederik <frederik.prijck@gmail.com>
2022-04-19 16:21:54 +0200

    Refactor assertHasProperties

--- auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/utils/assertHasProperties.kt
+++ auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/utils/assertHasProperties.kt
@@ -1,10 +1,18 @@
 package com.auth0.auth0_flutter.utils
 
+fun tryGetByKey(data: Any?, key: String): Any? {
+    if (data is Map<*, *>) {
+        return data[key];
+    }
+
+    return null;
+}
+
 fun assertHasProperties(requiredProperties: List<String>, data: Map<*, *>) {
     var missingProperties =
         requiredProperties.filter {
             it.split('.')
-                .fold(data) { acc: Any?, v: String -> (acc as Map<*, *>?)?.get(v) } == null
+                .fold(data) { acc: Any?, key: String -> tryGetByKey(acc, key) } == null
         };
 
     missingProperties.forEach { throw NullPointerException("Required property '$it' is not provided.") }
