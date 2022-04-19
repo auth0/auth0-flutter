5054b1b67e43420b35eb4574abc5a5716d655f38
Frederik <frederik.prijck@gmail.com>
2022-04-19 14:36:29 +0200

    Implement Telemetry in Android

--- auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/request_handlers/MethodCallRequest.kt
+++ auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/request_handlers/MethodCallRequest.kt
@@ -1,6 +1,7 @@
 package com.auth0.auth0_flutter.request_handlers
 
 import com.auth0.android.Auth0
+import com.auth0.android.util.Auth0UserAgent
 import com.auth0.auth0_flutter.utils.assertHasProperties
 import io.flutter.plugin.common.MethodCall
 
@@ -8,21 +9,39 @@ class MethodCallRequest {
     var account: Auth0;
     var data: HashMap<*, *>;
 
-    constructor(domain: String, clientId: String, data: HashMap<*, *>) {
+    constructor(account: Auth0, data: HashMap<*, *>) {
         this.data = data;
-        this.account = Auth0(
-            clientId,
-            domain
-        )
+        this.account = account;
+
     }
 
     companion object {
         fun fromCall(call: MethodCall): MethodCallRequest {
             val args = call.arguments as HashMap<*, *>;
 
-            assertHasProperties(listOf("domain", "clientId"), args);
+            assertHasProperties(
+                listOf(
+                    "domain",
+                    "clientId",
+                    "telemetry",
+                    "telemetry.name",
+                    "telemetry.version"
+                ), args
+            );
 
-            return MethodCallRequest(args["domain"] as String, args["clientId"] as String, args);
+            val account = Auth0(
+                args["clientId"] as String,
+                args["domain"] as String
+            )
+
+            val telemetry = args["telemetry"] as Map<String, String>;
+
+            account.auth0UserAgent = Auth0UserAgent(
+                name = telemetry["name"] as String,
+                version = telemetry["version"] as String,
+            )
+
+            return MethodCallRequest(account, args);
         }
     }
 }
--- auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/utils/assertHasProperties.kt
+++ auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/utils/assertHasProperties.kt
@@ -1,7 +1,11 @@
 package com.auth0.auth0_flutter.utils
 
-fun assertHasProperties (requiredProperties: List<String>, data: Map<*, *>) {
-    var missingProperties = requiredProperties.filter { data[it] == null };
+fun assertHasProperties(requiredProperties: List<String>, data: Map<*, *>) {
+    var missingProperties =
+        requiredProperties.filter {
+            it.split('.')
+                .fold(data) { acc: Any?, v: String -> (acc as Map<*, *>)[v]} == null
+        };
 
     missingProperties.forEach { throw NullPointerException("Required property '$it' is not provided.") }
 }
