f0bd6463bf5bd58f8ee44590dd7ae9ce27cad54a
Frederik <frederik.prijck@gmail.com>
2022-04-22 15:01:39 +0200

    Add missing Web Auth logout tests

--- auth0_flutter/android/src/test/kotlin/com/auth0/auth0_flutter/LogoutWebAuthRequestHandlerTest.kt
+++ auth0_flutter/android/src/test/kotlin/com/auth0/auth0_flutter/LogoutWebAuthRequestHandlerTest.kt
@@ -1,11 +1,14 @@
 package com.auth0.auth0_flutter
 
 import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationAPIClient
 import com.auth0.android.authentication.AuthenticationException
 import com.auth0.android.callback.Callback
 import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.request.AuthenticationRequest
 import com.auth0.android.result.Credentials
 import com.auth0.auth0_flutter.request_handlers.MethodCallRequest
+import com.auth0.auth0_flutter.request_handlers.api.LoginApiRequestHandler
 import com.auth0.auth0_flutter.request_handlers.web_auth.LoginWebAuthRequestHandler
 import com.auth0.auth0_flutter.request_handlers.web_auth.LogoutWebAuthRequestHandler
 import io.flutter.plugin.common.MethodChannel.Result
@@ -25,7 +28,7 @@ class LogoutWebAuthRequestHandlerTest {
     fun runHandler(args: HashMap<String, Any?> = hashMapOf(), resultCallback: (Result, WebAuthProvider.LogoutBuilder) -> Unit) {
         val mockBuilder = mock<WebAuthProvider.LogoutBuilder>()
         val mockResult = mock<Result>()
-        val handler = LogoutWebAuthRequestHandler(mockBuilder)
+        val handler = LogoutWebAuthRequestHandler { mockBuilder }
 
         doAnswer { invocation ->
             val callback = invocation.getArgument<Callback<Void?, AuthenticationException>>(1)
@@ -54,6 +57,15 @@ class LogoutWebAuthRequestHandlerTest {
     }
 
     @Test
+    fun `handler doesn't add scheme when not specified in the arguments`() {
+        val args = hashMapOf<String, Any?>()
+
+        runHandler(args) { _, builder ->
+            verify(builder, times(0)).withScheme(anyOrNull())
+        }
+    }
+
+    @Test
     fun `handler adds returnTo when specified in the arguments`() {
        val args = hashMapOf<String, Any?>("returnTo" to "http://return.to")
 
@@ -63,19 +75,52 @@ class LogoutWebAuthRequestHandlerTest {
     }
 
     @Test
+    fun `handler doesn't add returnTo when not specified in the arguments`() {
+        val args = hashMapOf<String, Any?>()
+
+        runHandler(args) { _, builder ->
+            verify(builder, times(0)).withReturnToUrl(anyOrNull())
+        }
+    }
+
+    @Test
+    fun `should call login with the correct parameters`() {
+        runHandler { _, builder ->
+            verify(builder).start(any(), any());
+        }
+    }
+
+    @Test
     fun `handler returns an error when the builder fails`() {
         val mockBuilder = mock<WebAuthProvider.LogoutBuilder>()
         val mockResult = mock<Result>()
-        val handler = LogoutWebAuthRequestHandler(mockBuilder)
+        val handler = LogoutWebAuthRequestHandler { mockBuilder }
+        val exception = AuthenticationException("code", "description")
 
         doAnswer { invocation ->
             val callback = invocation.getArgument<Callback<Void?, AuthenticationException>>(1)
-            val exception = AuthenticationException("code", "description")
 
             callback.onFailure(exception)
-            verify(mockResult).error("code", "description", exception)
         }.`when`(mockBuilder).start(any(), any())
 
         handler.handle(mock(), MethodCallRequest(Auth0("test-domain", "test-client"), mock()), mockResult)
+
+        verify(mockResult).error("code", "description", exception)
+    }
+
+    @Test
+    fun `handler returns the result when the builder succeeds`() {
+        val mockBuilder = mock<WebAuthProvider.LogoutBuilder>()
+        val mockResult = mock<Result>()
+        val handler = LogoutWebAuthRequestHandler { mockBuilder }
+
+        doAnswer { invocation ->
+            val callback = invocation.getArgument<Callback<Void?, AuthenticationException>>(1)
+            callback.onSuccess(null)
+        }.`when`(mockBuilder).start(any(), any())
+
+        handler.handle(mock(), MethodCallRequest(Auth0("test-domain", "test-client"), mock()), mockResult)
+
+        verify(mockResult).success(null)
     }
 }
