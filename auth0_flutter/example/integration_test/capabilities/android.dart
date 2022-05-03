93842b323c443d063797858ad99d52ccfcb4eeec
Frederik <frederik.prijck@gmail.com>
2022-05-03 12:10:46 +0200

    Revert deviceName

--- auth0_flutter/example/integration_test/capabilities/android.dart
+++ auth0_flutter/example/integration_test/capabilities/android.dart
@@ -3,6 +3,6 @@ import 'dart:io';
 final Map<String, dynamic> androidCapabilities = {
   'platformName': 'Android',
   'app': File('build/app/outputs/flutter-apk/app-debug.apk').absolute.path,
-  'deviceName': 'myavd',
+  'deviceName': 'emulator-5554',
   'automationName': 'Flutter',
 };
