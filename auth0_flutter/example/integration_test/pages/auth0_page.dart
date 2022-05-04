5ca701f15cea879f366be4e5f52cbe4347fa1c2b
frederikprijck <frederik.prijck@auth0.com>
2022-05-04 10:53:28 +0200

    Avoid delays in tests

--- auth0_flutter/example/integration_test/pages/auth0_page.dart
+++ auth0_flutter/example/integration_test/pages/auth0_page.dart
@@ -1,30 +1,47 @@
 import 'package:appium_driver/async_io.dart';
 
+import '../utils/wait_for.dart';
+
 class Auth0Page {
   AppiumWebDriver driver;
 
   Auth0Page({required this.driver});
 
-  Future<void> enterCredentialsAndSubmit(final String email, final String password) async {
-    await Future<dynamic>.delayed(const Duration(seconds: 5));
-    final emailInput = await driver
-        .findElements(const AppiumBy.className('android.widget.EditText'))
-        .elementAt(0);
-    final passwordInput = await driver
-        .findElements(const AppiumBy.className('android.widget.EditText'))
-        .elementAt(1);
+  Future<void> enterCredentialsAndSubmit(
+      final String email, final String password) async {
+    await waitForBrowser();
+
+    final emailInput = await getEmailInput();
+    final passwordInput = await getPasswordInput();
 
     await emailInput.sendKeys(email);
     await passwordInput.sendKeys(password);
 
-    await Future<dynamic>.delayed(const Duration(seconds: 2));
-
-    final loginButton = await driver
-        .findElements(const AppiumBy.className('android.widget.Button'))
-        .elementAt(1);
+    final loginButton = await getLoginButton();
 
     await loginButton.click();
+  }
 
-    await Future<dynamic>.delayed(const Duration(seconds: 5));
+  Future<void> waitForBrowser() async {
+    await waitForElement(
+        driver, const AppiumBy.className('android.webkit.WebView'));
+  }
+
+  Future<AppiumWebElement> getEmailInput() {
+    return driver
+        .findElements(const AppiumBy.className('android.widget.EditText'))
+        .elementAt(0);
+  }
+
+  Future<AppiumWebElement> getPasswordInput() {
+    return driver
+        .findElements(const AppiumBy.className('android.widget.EditText'))
+        .elementAt(1);
+  }
+
+  Future<AppiumWebElement> getLoginButton() {
+    return driver
+        .findElements(const AppiumBy.className('android.widget.Button'))
+        .elementAt(1);
   }
 }
--- auth0_flutter/example/integration_test/smoke_test.dart
+++ auth0_flutter/example/integration_test/smoke_test.dart
@@ -26,6 +26,8 @@ void main() {
     examplePage = ExamplePage(driver: driver);
     auth0Page = Auth0Page(driver: driver);
     systemPage = SystemPage(driver: driver);
+
+    driver.driver.timeouts.setImplicitTimeout(const Duration(seconds: 2));
   });
 
   tearDownAll(() async {
