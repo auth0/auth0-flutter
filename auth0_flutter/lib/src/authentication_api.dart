e2e6ca33e4b30dc741e3a299a00a7de8e863bb34
Steve Hobbs <steve.hobbs@hey.com>
2022-06-29 10:25:15 +0100

    [SDK-3451] SDK Documentation (#101)
    * [SDK-3437] Add NOTICE file (#100)
    
    * Doc comments for majority API
    
    * Add doc comments for Authentication API
    
    * Apply suggestions from code review
    
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>
    
    * Further docs changes from feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>
    
    * Documentation for redirectUrl
    
    * Documentation for returnTo
    
    * Apply suggestions from code review
    
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>
    
    * Update auth0_flutter/lib/src/web_authentication.dart
    
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>
    
    * Correct comment on Credentials.scopes
    
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>

--- auth0_flutter/lib/src/authentication_api.dart
+++ auth0_flutter/lib/src/authentication_api.dart
@@ -1,11 +1,55 @@
 import 'package:auth0_flutter_platform_interface/auth0_flutter_platform_interface.dart';
 
+/// An interface for calling some of the endpoints in [Auth0's Authentication API](https://auth0.com/docs/api/authentication).
+///
+/// This class presents building blocks for doing more fine-grained authentication with Auth0 using Username and Password login. Unlike
+/// [WebAuthentication], these do **not** use [Auth0 Universal Login](https://auth0.com/docs/authenticate/login/auth0-universal-login) (the recommended way of doing authentication),
+/// and thus users are not redirected to Auth0 for authentication.
+///
+/// It is not intended for you to instantiate this class yourself, as an instance of it is already exposed as [Auth0.api].
+///
+/// Usage example:
+///
+/// ```dart
+/// final auth0 = Auth0('DOMAIN', 'CLIENT_ID');
+///
+/// final result = await auth0.api.login({
+///   usernameOrEmail: 'my@email.com',
+///   password: 'my_password'
+///   connectionOrRealm: 'Username-Password-Authentication'
+/// })
+///
+/// final accessToken = result.accessToken;
+/// ```
 class AuthenticationApi {
   final Account _account;
   final UserAgent _userAgent;
 
   AuthenticationApi(this._account, this._userAgent);
 
+  /// Authenticates the user using a [usernameOrEmail] and a [password], with the specified [connectionOrRealm]. If successful, it returns
+  /// a set of tokens, as well as the user's profile (constructed from ID token claims).
+  ///
+  /// If using the default username and password database connection, [connectionOrRealm] should be set to `Username-Password-Authentication`.
+  ///
+  /// ## Endpoint docs
+  /// https://auth0.com/docs/api/authentication#login
+  ///
+  /// ## Notes
+  ///
+  /// * [audience] relates to the API Identifier you want to reference in your access tokens (see [API settings](https://auth0.com/docs/get-started/apis/api-settings))
+  /// * [scopes] defaults to `openid profile email`
+  /// * [parameters] can be used to sent through custom parameters to the endpoint to be picked up in a Rule or Action.
+  ///
+  /// ## Usage example
+  ///
+  /// ```dart
+  /// final result = await auth0.api.login({
+  ///   usernameOrEmail: 'my@email.com',
+  ///   password: 'my_password'
+  ///   connectionOrRealm: 'Username-Password-Authentication'
+  /// });
+  /// ```
   Future<Credentials> login({
     required final String usernameOrEmail,
     required final String password,
@@ -24,6 +68,22 @@ class AuthenticationApi {
         parameters: parameters,
       )));
 
+  /// Fetches the user's profile from the /userinfo endpoint. An [accessToken] from a successful authentication call must be supplied.
+  ///
+  /// ## Endpoint
+  /// https://auth0.com/docs/api/authentication#user-profile
+  ///
+  /// ## Usage example
+  ///
+  /// ```dart
+  /// final result = await auth0.api.login({
+  ///   usernameOrEmail: 'my@email.com',
+  ///   password: 'my_password'
+  ///   connectionOrRealm: 'Username-Password-Authentication'
+  /// });
+  ///
+  /// final profile = await auth0.api.userProfile({ accessToken: result.accessToken });
+  /// ```
   Future<UserProfile> userProfile(
           {required final String accessToken,
           final Map<String, String> parameters = const {}}) =>
@@ -31,6 +91,14 @@ class AuthenticationApi {
           AuthUserInfoOptions(
               accessToken: accessToken, parameters: parameters)));
 
+  /// Registers a new user with the specified [email] address and [password] in the specified [connection].
+  ///
+  /// Endpoint
+  /// https://auth0.com/docs/api/authentication#signup
+  ///
+  /// ## Notes
+  ///
+  /// * [username] is only required if the [connection] you specify requires it
   Future<DatabaseUser> signup(
           {required final String email,
           required final String password,
@@ -47,6 +115,32 @@ class AuthenticationApi {
               userMetadata: userMetadata,
               parameters: parameters)));
 
+  /// Uses a [refreshToken] to get a new access token.
+  ///
+  /// ## Endpoint
+  /// https://auth0.com/docs/api/authentication#refresh-token
+  ///
+  /// ## Notes
+  /// * Refresh tokens can be retrieved by specifying the `offline_access` scope during authentication.
+  /// * [scopes] can be specified if a reduced set of scopes is desired.
+  ///
+  /// ## Further reading
+  /// [Refresh Tokens on Auth0 docs](https://auth0.com/docs/secure/tokens/refresh-tokens)
+  ///
+  /// ## Usage example
+  ///
+  /// ```dart
+  /// final result = await auth0.api.login({
+  ///   usernameOrEmail: 'my@email.com',
+  ///   password: 'my_password'
+  ///   connectionOrRealm: 'Username-Password-Authentication',
+  ///   scopes: {'openid', 'profile', 'email', 'offline_access'}
+  /// });
+  ///
+  /// if (result.refreshToken != null) {
+  ///    await auth0.api.renewCredentials(refreshToken: result.refreshToken!);
+  /// }
+  /// ```
   Future<Credentials> renewCredentials({
     required final String refreshToken,
     final Set<String> scopes = const {},
@@ -58,6 +152,18 @@ class AuthenticationApi {
               scopes: scopes,
               parameters: parameters)));
 
+  /// Initiates a reset of password of the user with the specific [email] address in the specific [connection].
+  ///
+  /// ## Endpoint
+  /// https://auth0.com/docs/api/authentication#change-password
+  ///
+  /// ## Notes
+  ///
+  /// Calling this endpoint does not reset the user's password in itself, but it asks Auth0 to send the user
+  /// an email with a link they can use to reset their password on the web.
+  ///
+  /// Arbitrary [parameters] can be specified and then picked up in a custom Auth0 [Action](https://auth0.com/docs/customize/actions) or
+  ///  [Rule](https://auth0.com/docs/customize/rules).
   Future<void> resetPassword(
           {required final String email,
           required final String connection,
--- auth0_flutter_platform_interface/lib/src/credentials.dart
+++ auth0_flutter_platform_interface/lib/src/credentials.dart
@@ -1,11 +1,42 @@
 import '../auth0_flutter_platform_interface.dart';
 
+/// A collection of authentication artifacts obtained from Auth0 when a user logs in.
 class Credentials {
+  /// A [JSON Web Token](https://jwt.io/introduction) that contains the user information.
+  ///
+  /// **Important**: The [ID tokens](https://auth0.com/docs/security/tokens/id-tokens) obtained from Web Auth login are automatically validated by the underlying native SDK, ensuring their
+  /// contents have not been tampered with.
+  ///
+  /// **This is not the case for the ID tokens obtained when using the authentication API directly.**
+  ///
+  /// You must [validate ID tokens](https://auth0.com/docs/security/tokens/id-tokens/validate-id-tokens) received from the Authentication API client before using the information they contain.
   final String idToken;
+
+  /// Token that can be used to make authenticated requests to the specified API (the **audience** value used on login).
+  ///
+  /// ## Futher reading
+  /// - [Access tokens](https://auth0.com/docs/security/tokens/access-tokens)
+  /// - [Audience](https://auth0.com/docs/secure/tokens/access-tokens/get-access-tokens#control-access-token-audience)
   final String accessToken;
+
+  /// Token that can be used to request a new access token.
+  ///
+  /// The scope `offline_access` must have been requested on login for a refresh token to be returned.
+  ///
+  /// [Read more about refresh tokens](https://auth0.com/docs/secure/tokens/refresh-tokens).
   final String? refreshToken;
+
+  /// The absolute date and time of when the access token expires.
   final DateTime expiresAt;
+
+  /// The scopes that have been granted by Auth0.
+  ///
+  /// [Read more about scopes](https://auth0.com/docs/get-started/apis/scopes).
   final Set<String> scopes;
+
+  /// Properties and attributes relating to the authenticated user.
+  ///
+  /// [Read more about Auth0 User Profiles](https://auth0.com/docs/manage-users/user-accounts/user-profiles)
   final UserProfile userProfile;
 
   Credentials({
