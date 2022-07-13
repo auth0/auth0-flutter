f5e814800f6a258caa5686a4502e362db5e6f457
Rita Zerrizuela <zeta@widcket.com>
2022-07-13 05:06:28 -0300

    Update `flutter_lints` to v2 (#117)

--- auth0_flutter/example/lib/main.dart
+++ auth0_flutter/example/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+// ignore: depend_on_referenced_packages
 import 'package:flutter_driver/driver_extension.dart';
 
 import 'example_app.dart';
--- auth0_flutter_platform_interface/lib/src/web-auth/id_token_validation_config.dart
+++ auth0_flutter_platform_interface/lib/src/web-auth/id_token_validation_config.dart
@@ -3,7 +3,7 @@ class IdTokenValidationConfig {
   /// The value in seconds used to account for clock skew in JWT expirations. Typically, this value is no more than a minute or two at maximum. Defaults to 60 seconds.
   final int? leeway;
 
-  /// The issuer to be used for validation of JWTs. Defaults to the domain used to when calling [Auth0.new].
+  /// The issuer to be used for validation of JWTs. Defaults to the domain used to when calling `Auth0.new`.
   final String? issuer;
 
   /// Maximum allowable elasped time (in seconds) since authentication. If the last time the user authenticated is greater than this value, the user must be reauthenticated. Defaults to 0.
