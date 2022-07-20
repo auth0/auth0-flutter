31bd6ec8e5ca55bae357fca03d84eb98fc1a4b8e
frederikprijck <frederik.prijck@auth0.com>
2022-07-20 08:59:24 +0200

    Remove appium tests

--- auth0_flutter/example/lib/main.dart
+++ auth0_flutter/example/lib/main.dart
@@ -2,14 +2,10 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-// ignore: depend_on_referenced_packages
-import 'package:flutter_driver/driver_extension.dart';
 
 import 'example_app.dart';
 
 Future<dynamic> main() async {
-  enableFlutterDriverExtension();
-
   await dotenv.load();
   runApp(const ExampleApp());
 }
