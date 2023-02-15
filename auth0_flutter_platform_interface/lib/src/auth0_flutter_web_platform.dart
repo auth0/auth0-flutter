8a315a275711d8020afe3301ccc1f114ea66be60
Rita Zerrizuela <zeta@widcket.com>
2023-02-14 23:35:33 -0300

    Rename stub platform class

--- auth0_flutter_platform_interface/lib/src/auth0_flutter_web_platform.dart
+++ auth0_flutter_platform_interface/lib/src/auth0_flutter_web_platform.dart
@@ -1,13 +1,13 @@
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class NoOpAuth0FlutterWeb extends Auth0FlutterWebPlatform {}
+class StubAuth0FlutterWeb extends Auth0FlutterWebPlatform {}
 
 abstract class Auth0FlutterWebPlatform extends PlatformInterface {
   Auth0FlutterWebPlatform() : super(token: _token);
 
   static Auth0FlutterWebPlatform get instance => _instance;
   static final Object _token = Object();
-  static Auth0FlutterWebPlatform _instance = NoOpAuth0FlutterWeb();
+  static Auth0FlutterWebPlatform _instance = StubAuth0FlutterWeb();
   static set instance(final Auth0FlutterWebPlatform instance) {
     PlatformInterface.verifyToken(instance, _token);
     _instance = instance;
