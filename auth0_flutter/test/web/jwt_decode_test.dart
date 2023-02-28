3f64b6094107827ef14882e868d8144acbc200ee
Rita Zerrizuela <zeta@widcket.com>
2023-02-27 21:31:28 -0300

    Extract utility function

--- auth0_flutter/test/web/jwt_decode_test.dart
+++ auth0_flutter/test/web/jwt_decode_test.dart
@@ -8,17 +8,16 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('JWTDecode', () {
     test('decodes a JWT payload', () async {
-      const jwt = 'HEADER.eyJzdWIiOiJjb20uc29td2hlcmUuZmFyLmJleW9uZDphcGkiLCJpc'
-          '3MiOiJhdXRoMCIsInVzZXJfcm9sZSI6ImFkbWluIn0.SIGNATURE';
-      final result = JWT.decode(jwt);
+      const payload = 'eyJzdWIiOiJjb20uc29td2hlcmUuZmFyLmJleW9uZDphcGkiLCJpc3Mi'
+          'OiJhdXRoMCIsInVzZXJfcm9sZSI6ImFkbWluIn0';
+      final result = JWT.decode(testJWT(payload));
 
       expect(result, isNotNull);
     });
 
     test('decodes an empty JWT payload', () async {
       const payload = 'e30';
-      const jwt = 'HEADER.$payload.SIGNATURE';
-      final result = JWT.decode(jwt);
+      final result = JWT.decode(testJWT(payload));
 
       expect(result, isNotNull);
     });
@@ -26,30 +25,27 @@ void main() {
     test('returns the decoded payload', () async {
       const data = {'sub': 1, 'foo': 'bar'};
       final payload = base64Url.encode(utf8.encode(jsonEncode(data)));
-      final jwt = 'HEADER.$payload.SIGNATURE';
-      final result = JWT.decode(jwt);
+      final result = JWT.decode(testJWT(payload));
 
       expect(result, equals(data));
     });
 
     test('throws an exception with an invalid Base64URL payload', () async {
       const payload = '+'; // Valid Base64, invalid Base64URL
-      const jwt = 'HEADER.$payload.SIGNATURE';
       const expectedCode = 'INVALID_BASE64URL';
       const expectedMessage = 'Failed to decode Base64URL value $payload.';
 
-      expect(() => JWT.decode(jwt),
+      expect(() => JWT.decode(testJWT(payload)),
           throwsJWTDecodeException(expectedCode, expectedMessage));
     });
 
     test('throws an exception with an invalid JSON payload', () async {
       const payload = 'Qk9EWQ==';
-      const jwt = 'HEADER.$payload.SIGNATURE';
       const expectedCode = 'INVALID_JSON';
       const expectedMessage =
           'Failed to parse JSON from Base64URL value $payload.';
 
-      expect(() => JWT.decode(jwt),
+      expect(() => JWT.decode(testJWT(payload)),
           throwsJWTDecodeException(expectedCode, expectedMessage));
     });
 
@@ -64,6 +60,8 @@ void main() {
     });
   });
 }
+
+String testJWT(final String payload) => 'HEADER.$payload.SIGNATURE';
 
 Matcher throwsJWTDecodeException(final String code, final String message) =>
     throwsA(Auth0ExceptionMatcher<JWTDecodeException>(code, message));
