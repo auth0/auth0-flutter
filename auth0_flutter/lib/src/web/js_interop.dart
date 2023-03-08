2f23ff09ddb9d1755354d469bb32328a240dd99c
Steve Hobbs <steve.hobbs@hey.com>
2023-03-08 23:03:37 +0000

    chore: remove unused js interop promise interfaces

--- auth0_flutter/lib/src/web/js_interop.dart
+++ auth0_flutter/lib/src/web/js_interop.dart
@@ -5,22 +5,6 @@ library auth0;
 
 import 'package:js/js.dart';
 
-typedef Resolver<T> = void Function(T);
-typedef Rejecter = void Function(dynamic);
-
-@JS()
-class Promise<T> extends Thenable<T> {
-  external Promise(
-      final void Function(Resolver<T> resolve, Rejecter reject) callback);
-  external static Promise<T> resolve<T>([final T value]);
-  external static Promise<T> reject<T>(final dynamic error);
-}
-
-@JS()
-class Thenable<T> {
-  external Thenable<T> then(final Resolver<T> resolve, [final Rejecter reject]);
-}
-
 @JS()
 @anonymous
 class AuthorizationParams {
@@ -144,11 +128,10 @@ class WebCredentials {
 @JS()
 class Auth0Client {
   external Auth0Client(final Auth0ClientOptions options);
-  external Promise<void> loginWithRedirect(
-      [final RedirectLoginOptions options]);
-  external Promise<void> handleRedirectCallback([final String? url]);
-  external Promise<void> checkSession();
-  external Promise<WebCredentials> getTokenSilently(
+  external Future<void> loginWithRedirect([final RedirectLoginOptions options]);
+  external Future<void> handleRedirectCallback([final String? url]);
+  external Future<void> checkSession();
+  external Future<WebCredentials> getTokenSilently(
       [final GetTokenSilentlyOptions? options]);
-  external Promise<bool> isAuthenticated();
+  external Future<bool> isAuthenticated();
 }
