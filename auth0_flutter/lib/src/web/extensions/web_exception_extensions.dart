ac5f71bce4bcf7395423dcb8ef5ce55ef27b40e8
Steve Hobbs <steve.hobbs@hey.com>
2023-03-17 13:49:41 +0000

    feat: support mfa_required exception

--- auth0_flutter/lib/src/web/extensions/web_exception_extensions.dart
+++ auth0_flutter/lib/src/web/extensions/web_exception_extensions.dart
@@ -1,8 +1,25 @@
+import 'dart:js';
 import 'dart:js_util';
 import 'package:auth0_flutter_platform_interface/auth0_flutter_platform_interface.dart';
 
 extension WebExceptionExtension on WebException {
-  static WebException fromJsObject(final Object jsException) => WebException(
-      getProperty(jsException, 'error'),
-      getProperty(jsException, 'error_description'), const {});
+  static WebException fromJsObject(final Object jsException) {
+    final error = getProperty<String>(jsException, 'error');
+    final description = getProperty<String>(jsException, 'error_description');
+    final Map<String, dynamic> details = {};
+
+    objectKeys(jsException).forEach((final key) {
+      if (key == 'error' || key == 'error_description') return;
+      details[key as String] = getProperty<dynamic>(jsException, key);
+    });
+
+    switch (error) {
+      case 'mfa_required':
+        return WebException.mfaError(
+            description, getProperty(jsException, 'mfaToken'));
+    }
+
+    return WebException(getProperty(jsException, 'error'),
+        getProperty(jsException, 'error_description'), details);
+  }
 }
--- auth0_flutter_platform_interface/lib/src/web/web_exception.dart
+++ auth0_flutter_platform_interface/lib/src/web/web_exception.dart
@@ -3,7 +3,12 @@ import '../../auth0_flutter_platform_interface.dart';
 /// Exception thrown by [Auth0FlutterWebPlatform] when something
 /// goes wrong.
 class WebException extends Auth0Exception {
+  static const String _mfaRequired = 'MFA_REQUIRED';
+
   const WebException(final String error, final String errorDescription,
       final Map<String, dynamic> details)
       : super(error, errorDescription, details);
+
+  WebException.mfaError(final String message, final String mfaToken)
+      : this(WebException._mfaRequired, message, {'mfaToken': mfaToken});
 }
