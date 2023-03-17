92907849247f5350625ffe2da82f89e5495ddc30
Steve Hobbs <steve.hobbs@hey.com>
2023-03-17 07:09:33 +0000

    chore: doc comments for WebException

--- auth0_flutter_platform_interface/lib/src/web/web_exception.dart
+++ auth0_flutter_platform_interface/lib/src/web/web_exception.dart
@@ -1,5 +1,7 @@
 import '../../auth0_flutter_platform_interface.dart';
 
+/// Exception thrown by [Auth0FlutterWebPlatform] when something
+/// goes wrong.
 class WebException extends Auth0Exception {
   const WebException(final String error, final String errorDescription,
       final Map<String, dynamic> details)
