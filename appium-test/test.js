81889a6e5c63f91ab87f5376b3e3b9850c348eb3
Rita Zerrizuela <zeta@widcket.com>
2023-08-31 01:18:49 +0100

    Remove autoGrantPermissions capability

--- appium-test/test.js
+++ appium-test/test.js
@@ -6,9 +6,7 @@ const capabilities = {
   'appium:deviceName': 'Android',
   'appium:appPackage': 'com.auth0.auth0_flutter_example',
   'appium:appActivity': '.MainActivity',
-  'appium:app': `${__dirname}/../auth0_flutter/example/build/app/outputs/flutter-apk/app-x86_64-release.apk`,
-  'appium:autoGrantPermissions': true,
-  'appium:ignoreHiddenApiPolicyError': true
+  'appium:app': `${__dirname}/../auth0_flutter/example/build/app/outputs/flutter-apk/app-x86_64-release.apk`
 };
 
 const wdOpts = {
