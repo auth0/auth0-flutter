d0a994dda8dd60e5d1c7d526058890a0a6168e91
Poovamraj T T <poovamraj@gmail.com>
2023-09-14 22:58:40 +0530

    Update to fix build

--- appium-test/test.js
+++ appium-test/test.js
@@ -6,7 +6,6 @@ const capabilities = {
   'appium:deviceName': 'Android',
   'appium:appPackage': 'com.auth0.auth0_flutter_example',
   'appium:appActivity': '.MainActivity',
-  // 'appium:app': `/Users/poovamrajthanganadarthiagarajan/repositories/auth0-flutter/auth0_flutter/example/build/app/outputs/flutter-apk/app-release.apk`
   'appium:app': `${__dirname}/../auth0_flutter/example/build/app/outputs/flutter-apk/app-x86_64-release.apk`
 };
 
@@ -24,17 +23,17 @@ async function runTest() {
     const loginButton = await driver.$('//android.widget.Button[@content-desc="Web Auth Login"]');
     await loginButton.click();
 
-    // const emailTextField = await driver.$("//android.widget.EditText[@hint='User name username/email']");
-    const emailTextField = await driver.$("//android.widget.EditText[@resource-id='username']");
+    const emailTextField = await driver.$("//android.widget.EditText[@hint='User name username/email']");
+    // const emailTextField = await driver.$("//android.widget.EditText[@resource-id='username']"); //For new universal login
     await emailTextField.waitForExist();
     await emailTextField.setValue(process.env.USER_EMAIL);
 
-    // const passwordTextField = await driver.$("//android.widget.EditText[@hint='Password your password']");
-    const passwordTextField = await driver.$("//android.widget.EditText[@resource-id='password']");
+    const passwordTextField = await driver.$("//android.widget.EditText[@hint='Password your password']");
+    // const passwordTextField = await driver.$("//android.widget.EditText[@resource-id='password']"); //For new universal login
     await passwordTextField.setValue(process.env.USER_PASSWORD);
 
-    // const continueButton = await driver.$("//android.widget.Button[@text='Log In']");
-    const continueButton = await driver.$("//android.widget.Button[@text='Continue']");
+    const continueButton = await driver.$("//android.widget.Button[@text='Log In']");
+    // const continueButton = await driver.$("//android.widget.Button[@text='Continue']"); //For new universal login
     await continueButton.click();
     
     const logoutButton = await driver.$('//android.widget.Button[@content-desc="Web Auth Logout"]');
