9cfa40d299ca6b7f3a977b10977ea16bbfef8d44
Rita Zerrizuela <zeta@widcket.com>
2023-11-21 18:59:04 +0000

    Add macOS support to the example app

--- auth0_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ auth0_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import auth0_flutter
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  Auth0FlutterPlugin.register(with: registry.registrar(forPlugin: "Auth0FlutterPlugin"))
 }
