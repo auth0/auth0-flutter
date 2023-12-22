391a9d2eef17caac5fd54d94b1f8b28d225ad6b7
José Quintas <jose.quintas@lepaya.com>
2023-12-22 13:12:52 +0100

    feat: add allowInterop to logout openUrl

--- auth0_flutter/lib/src/web/extensions/logout_options.extension.dart
+++ auth0_flutter/lib/src/web/extensions/logout_options.extension.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:auth0_flutter_platform_interface/auth0_flutter_platform_interface.dart';
 
 import '../js_interop.dart' as interop;
@@ -6,7 +8,7 @@ import '../js_interop_utils.dart';
 extension LogoutOptionsExtension on LogoutOptions {
   interop.LogoutOptions toClientLogoutOptions() =>
       JsInteropUtils.stripNulls(interop.LogoutOptions(
-        openUrl: openUrl,
+        openUrl: openUrl != null ? allowInterop(openUrl!) : null,
         logoutParams: JsInteropUtils.stripNulls(
             interop.LogoutParams(federated: federated, returnTo: returnTo)),
       ));
