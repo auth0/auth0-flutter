924f6765af2f413b91a588462274bc547dabf1a0
Navaron Bracke <brackenavaron@gmail.com>
2024-11-21 11:53:46 +0100

    omit argument when not provided

--- auth0_flutter/lib/src/web/auth0_flutter_web_platform_proxy.dart
+++ auth0_flutter/lib/src/web/auth0_flutter_web_platform_proxy.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 import 'dart:js_util';
 import 'js_interop.dart';
 
@@ -21,10 +20,14 @@ class Auth0FlutterWebClientProxy {
           [final GetTokenSilentlyOptions? options]) =>
       promiseToFuture(client.getTokenSilently(options));
 
-  Future<RedirectLoginResult> handleRedirectCallback([final String? url]) =>
-      promiseToFuture(client.handleRedirectCallback(
-        url ?? window.location.href,
-      ));
+  Future<RedirectLoginResult> handleRedirectCallback([final String? url]) {
+    // Omit the url if it is not provided, so that the default argument is used.
+    if (url == null) {
+      return promiseToFuture(client.handleRedirectCallback());
+    } else {
+      return promiseToFuture(client.handleRedirectCallback(url));
+    }
+  }
 
   Future<bool> isAuthenticated() => promiseToFuture(client.isAuthenticated());
 
