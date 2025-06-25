002b8d699868482954d21e74a60bd9bb41672692
Nandan Prabhu <nandan.prabhup@okta.com>
2025-06-25 10:16:15 +0530

    SDK-6070 flutter changes
    (cherry picked from commit 1cb05d808b39ef82b7cac6e042bc7cf30ba6e33a)

--- auth0_flutter_platform_interface/lib/src/credentials-manager/method_channel_credentials_manager.dart
+++ auth0_flutter_platform_interface/lib/src/credentials-manager/method_channel_credentials_manager.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart';
 
+import '../user_info.dart';
 import '../credentials.dart';
 import '../request/request.dart';
 import '../request/request_options.dart';
@@ -15,6 +16,8 @@ const String credentialsManagerSaveCredentialsMethod =
     'credentialsManager#saveCredentials';
 const String credentialsManagerGetCredentialsMethod =
     'credentialsManager#getCredentials';
+const String credentialsManagerGetUserProfileMethod =
+'credentialsManager#getIDTokenContent';
 const String credentialsManagerClearCredentialsMethod =
     'credentialsManager#clearCredentials';
 const String credentialsManagerHasValidCredentialsMethod =
@@ -46,6 +49,12 @@ class MethodChannelCredentialsManager extends CredentialsManagerPlatform {
     final bool? result = await _invokeRequest<bool, RequestOptions?>(
         method: credentialsManagerSaveCredentialsMethod, request: request);
     return result ?? true;
+  }
+
+  @override
+  Future<UserInfo> getIDTokenContents(final CredentialsManagerRequest request) async {
+      final Map<String, dynamic> result = await _invokeMapRequest(method: credentialsManagerGetUserProfileMethod, request: request);
+      return UserInfo.fromJson(result);
   }
 
   /// Removes the credentials from the native storage if present.
