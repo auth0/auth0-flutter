f2d086688d967b9b5cdc206b91d204aee2734946
Nandan Prabhu <nandan.prabhup@okta.com>
2025-08-21 05:55:27 +0000

    windows support fixes build issue due to dependencies

--- auth0_flutter/example/windows/flutter/generated_plugin_registrant.cc
+++ auth0_flutter/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <auth0_flutter/auth0_flutter_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  Auth0FlutterPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("Auth0FlutterPluginCApi"));
 }
