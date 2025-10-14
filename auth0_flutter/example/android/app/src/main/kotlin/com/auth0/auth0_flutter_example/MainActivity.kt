0fb02f4f1170c225a91f8dc52ab22f5a5918768a
utkrishtS <utkrisht.sahu@okta.com>
2025-10-14 10:24:15 +0530

    feat(android): Upgrade native SDK to 3.9.0 and adapt to breaking changes

--- auth0_flutter/example/android/app/src/main/kotlin/com/auth0/auth0_flutter_example/MainActivity.kt
+++ auth0_flutter/example/android/app/src/main/kotlin/com/auth0/auth0_flutter_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.auth0.auth0_flutter_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
