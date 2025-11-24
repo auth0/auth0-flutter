52be1b51fff802c898692aa2bd003b98c1e260c3
Trevor Dunn <trevordunn@users.noreply.github.com>
2025-11-24 09:52:20 -0700

    Fix merge mistakes in lib/src/web/auth0_flutter_plugin_real.dart

--- auth0_flutter/lib/src/web/auth0_flutter_plugin_real.dart
+++ auth0_flutter/lib/src/web/auth0_flutter_plugin_real.dart
@@ -3,7 +3,6 @@ import 'dart:js_interop';
 
 import 'package:auth0_flutter_platform_interface/auth0_flutter_platform_interface.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'package:js/js.dart';
 import 'package:web/web.dart';
 
 import 'auth0_flutter_web_platform_proxy.dart';
@@ -40,19 +39,23 @@ class Auth0FlutterPlugin extends Auth0FlutterWebPlatform {
   Future<Object?> get appState => Future<Object?>.value(_appState);
 
   @override
-  Future<void> initialize(final ClientOptions clientOptions, final UserAgent userAgent) async {
+  Future<void> initialize(
+      final ClientOptions clientOptions, final UserAgent userAgent) async {
     clientProxy ??= Auth0FlutterWebClientProxy(
       client: interop.Auth0Client(
-        JsInteropUtils.stripNulls(clientOptions.toAuth0ClientOptions(userAgent)),
+        JsInteropUtils.stripNulls(
+            clientOptions.toAuth0ClientOptions(userAgent)),
       ),
     );
 
     final search = urlSearchProvider();
 
     if (search?.contains('state=') == true &&
-        (search?.contains('code=') == true || search?.contains('error=') == true)) {
+        (search?.contains('code=') == true ||
+            search?.contains('error=') == true)) {
       try {
-        final interop.RedirectLoginResult result = await clientProxy!.handleRedirectCallback();
+        final interop.RedirectLoginResult result =
+            await clientProxy!.handleRedirectCallback();
 
         _appState = result.appState.dartify();
 
@@ -79,13 +82,20 @@ class Auth0FlutterPlugin extends Auth0FlutterWebPlatform {
           organization: options?.organizationId,
           invitation: invitationTicket,
           max_age: options?.idTokenValidationConfig?.maxAge,
-          scope: options?.scopes.isNotEmpty == true ? options?.scopes.join(' ') : null,
+          scope: options?.scopes.isNotEmpty == true
+              ? options?.scopes.join(' ')
+              : null,
         ),
         options?.parameters ?? {},
       ),
     );
 
-    final loginOptions = interop.RedirectLoginOptions(authorizationParams: authParams);
+    final openUrl = options?.openUrl;
+
+    final loginOptions = interop.RedirectLoginOptions(
+      authorizationParams: authParams,
+      openUrl: openUrl,
+    );
 
     return client.loginWithRedirect(loginOptions);
   }
@@ -102,7 +112,9 @@ class Auth0FlutterPlugin extends Auth0FlutterWebPlatform {
           organization: options?.organizationId,
           invitation: invitationTicket,
           max_age: options?.idTokenValidationConfig?.maxAge,
-          scope: options?.scopes.isNotEmpty == true ? options?.scopes.join(' ') : null,
+          scope: options?.scopes.isNotEmpty == true
+              ? options?.scopes.join(' ')
+              : null,
         ),
         options?.parameters ?? {},
       ),
@@ -150,7 +162,8 @@ class Auth0FlutterPlugin extends Auth0FlutterWebPlatform {
   @override
   Future<Credentials> credentials(final CredentialsOptions? options) async {
     final clientProxy = _ensureClient();
-    final tokenOptions = options?.toGetTokenSilentlyOptions() ?? interop.GetTokenSilentlyOptions();
+    final tokenOptions = options?.toGetTokenSilentlyOptions() ??
+        interop.GetTokenSilentlyOptions();
     // Force this, as we always want the full detail back so that we can
     // return a full Credentials instance.
     tokenOptions.detailedResponse = true;
