e3e6cb6ed129cb8e7fab36ad83e3352775da02b3
utkrishtS <utkrisht.sahu@okta.com>
2025-12-04 01:46:43 +0530

    Fix CI failures: Add missing files and update test mocks

---
+++ auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/request_handlers/api/UtilityRequestHandler.kt
@@ -0,0 +1,13 @@
+package com.auth0.auth0_flutter.request_handlers.api
+
+import com.auth0.auth0_flutter.request_handlers.MethodCallRequest
+import io.flutter.plugin.common.MethodChannel
+
+/**
+ * Interface for request handlers that don't require an AuthenticationAPIClient instance.
+ * Used for utility operations like DPoP key management that use static methods.
+ */
+interface UtilityRequestHandler {
+    val method: String
+    fun handle(request: MethodCallRequest, result: MethodChannel.Result)
+}
---
+++ auth0_flutter/ios/Classes/DPoP/DPoPHandler.swift
@@ -0,0 +1,1 @@
+../../../darwin/Classes/DPoP/DPoPHandler.swift
