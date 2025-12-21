756394ad1d6d1c2f01f39c67a04a3fdc267e312e
Nandan Prabhu <nandan.prabhup@okta.com>
2025-12-21 18:20:57 +0000

    Fixes issue due to expires in
    (cherry picked from commit 46f28b52ab11d7f8c01bfb3574be286b242ccfd4)

--- auth0_flutter/windows/time_util.h
+++ auth0_flutter/windows/time_util.h
@@ -1,6 +1,11 @@
 #pragma once
+
 #include <string>
+#include <optional>
 #include <chrono>
 
-std::string ToIso8601(
-    const std::chrono::system_clock::time_point& tp);
+std::optional<std::chrono::system_clock::time_point>
+ParseIso8601(const std::string& iso);
+
+std::string
+ToIso8601(const std::chrono::system_clock::time_point& tp);
