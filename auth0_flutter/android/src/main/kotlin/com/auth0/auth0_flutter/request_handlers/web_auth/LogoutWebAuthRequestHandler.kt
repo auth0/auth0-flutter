5a97d0abe9ff5dc56d3cf30046f10f6ee2dfa085
Prince Mathew <17837162+pmathew92@users.noreply.github.com>
2026-01-21 17:08:12 +0530

    feat: Add allowedBrowsers parameter to logout API [SDK-724] (#726)

--- auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/request_handlers/web_auth/LogoutWebAuthRequestHandler.kt
+++ auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/request_handlers/web_auth/LogoutWebAuthRequestHandler.kt
@@ -3,14 +3,21 @@ package com.auth0.auth0_flutter.request_handlers.web_auth
 import android.content.Context
 import com.auth0.android.authentication.AuthenticationException
 import com.auth0.android.callback.Callback
+import com.auth0.android.provider.BrowserPicker
+import com.auth0.android.provider.CustomTabsOptions
 import com.auth0.android.provider.WebAuthProvider
 import com.auth0.auth0_flutter.request_handlers.MethodCallRequest
 import io.flutter.plugin.common.MethodChannel
 
-class LogoutWebAuthRequestHandler(private val builderResolver: (MethodCallRequest) -> WebAuthProvider.LogoutBuilder) : WebAuthRequestHandler {
+class LogoutWebAuthRequestHandler(private val builderResolver: (MethodCallRequest) -> WebAuthProvider.LogoutBuilder) :
+    WebAuthRequestHandler {
     override val method: String = "webAuth#logout"
 
-    override fun handle(context: Context, request: MethodCallRequest, result: MethodChannel.Result) {
+    override fun handle(
+        context: Context,
+        request: MethodCallRequest,
+        result: MethodChannel.Result
+    ) {
         val builder = builderResolver(request)
         val args = request.data
 
@@ -26,7 +33,18 @@ class LogoutWebAuthRequestHandler(private val builderResolver: (MethodCallReques
             builder.withFederated()
         }
 
-        builder.start(context, object: Callback<Void?, AuthenticationException> {
+        val allowedBrowsers =
+            (args["allowedBrowsers"] as? List<*>)?.filterIsInstance<String>().orEmpty()
+        if (allowedBrowsers.isNotEmpty()) {
+            builder.withCustomTabsOptions(
+                CustomTabsOptions.newBuilder().withBrowserPicker(
+                    BrowserPicker.newBuilder()
+                        .withAllowedPackages(allowedBrowsers).build()
+                ).build()
+            )
+        }
+
+        builder.start(context, object : Callback<Void?, AuthenticationException> {
             override fun onFailure(exception: AuthenticationException) {
                 result.error(exception.getCode(), exception.getDescription(), exception)
             }
