807dcae2cdacb3bc1b2246ad2390dfb3920edf7d
sanchitmehtagit <sanchit.mehta@okta.com>
2026-02-27 19:07:45 +0530

    feat: Implement Native to Web (#749)

---
+++ auth0_flutter/android/src/main/kotlin/com/auth0/auth0_flutter/request_handlers/credentials_manager/GetSSOCredentialsRequestHandler.kt
@@ -0,0 +1,40 @@
+package com.auth0.auth0_flutter.request_handlers.credentials_manager
+
+import android.content.Context
+import com.auth0.android.authentication.storage.CredentialsManagerException
+import com.auth0.android.authentication.storage.SecureCredentialsManager
+import com.auth0.android.callback.Callback
+import com.auth0.android.result.SSOCredentials
+import com.auth0.auth0_flutter.request_handlers.MethodCallRequest
+import io.flutter.plugin.common.MethodChannel
+
+class GetSSOCredentialsRequestHandler : CredentialsManagerRequestHandler {
+    override val method: String = "credentialsManager#getSSOCredentials"
+
+    override fun handle(
+        credentialsManager: SecureCredentialsManager,
+        context: Context,
+        request: MethodCallRequest,
+        result: MethodChannel.Result
+    ) {
+        val parameters = request.data["parameters"] as Map<String, String>? ?: mapOf()
+
+        credentialsManager.getSsoCredentials(parameters, object :
+            Callback<SSOCredentials, CredentialsManagerException> {
+            override fun onFailure(exception: CredentialsManagerException) {
+                result.error(exception.message ?: "UNKNOWN ERROR", exception.message, exception)
+            }
+
+            override fun onSuccess(credentials: SSOCredentials) {
+                val map = mutableMapOf<String, Any?>(
+                    "sessionTransferToken" to credentials.sessionTransferToken,
+                    "tokenType" to credentials.issuedTokenType,
+                    "expiresIn" to credentials.expiresIn,
+                    "idToken" to credentials.idToken
+                )
+                credentials.refreshToken?.let { map["refreshToken"] = it }
+                result.success(map)
+            }
+        })
+    }
+}
---
+++ auth0_flutter_platform_interface/lib/src/credentials-manager/options/get_sso_credentials_options.dart
@@ -0,0 +1,23 @@
+import '../../../auth0_flutter_platform_interface.dart';
+
+// ignore: comment_references
+/// Options used to retrieve [SSOCredentials] via
+/// `CredentialsManagerPlatform.getSSOCredentials()`.
+class GetSSOCredentialsOptions implements RequestOptions {
+  /// Additional parameters to include in the SSO credentials request.
+  final Map<String, String> parameters;
+
+  /// Additional headers to include in the SSO credentials request.
+  final Map<String, String> headers;
+
+  GetSSOCredentialsOptions({
+    this.parameters = const {},
+    this.headers = const {},
+  });
+
+  @override
+  Map<String, dynamic> toMap() => {
+        'parameters': parameters,
+        'headers': headers,
+      };
+}
