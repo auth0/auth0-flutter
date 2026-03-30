be1f81c0ff27247fdc652e352218d19fdb52b94e
nandan_prabhu <nandan.prabhup@okta.com>
2026-03-30 19:04:50 +0530

    chore: Release readiness (#798)

--- auth0_flutter/windows/oauth_helpers.cpp
+++ auth0_flutter/windows/oauth_helpers.cpp
@@ -16,6 +16,7 @@
 #include <vector>
 #include <sstream>
 #include <iomanip>
+#include <pplx/pplxtasks.h>
 
 // OpenSSL for PKCE
 #include <openssl/sha.h>
@@ -213,7 +214,7 @@ namespace auth0_flutter
         {
             if (ct.is_canceled())
             {
-                pplx::cancel_current_task();
+                throw pplx::task_canceled();
             }
 
             std::string uri = readAndClearEnv();
@@ -338,7 +339,7 @@ namespace auth0_flutter
         {
             if (ct.is_canceled())
             {
-                pplx::cancel_current_task();
+                throw pplx::task_canceled();
             }
 
             std::string uri = readAndClearEnv();
